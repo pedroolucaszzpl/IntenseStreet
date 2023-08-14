11aa1d70d4c504b655b777004d258fc70a73bc00
pedroolucaszzpl <125031536+pedroolucaszzpl@users.noreply.github.com>
2023-08-14 14:13:48 -0300

    restau

--- js/login.js
+++ js/login.js
@@ -3,7 +3,6 @@ function Logar(){
     var senha = '0000';
  
     if(document.getElementById('nome_login').value == usuario && document.getElementById('senha_login').value == senha){
-        alert("Senha correta, entrando no sistema...")
         window.location.href = "main.html"
     }else{
         alert("Senha incorreta, tente novamente...")
