1c4699b3e4d0cdf9cb21f15173629464338980df
TaiMenegatti-dev <tainam.vieira@gmail.com>
2023-08-28 15:29:23 -0300

    php

---
+++ main.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>IntenseStreet HomePage</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <a class="navbar-logo" href="index.html">
+                <img src="img/logo1.png" alt="Logo IntenseStreet" description="Logo IntenseStreet" id="logo1">
+            </a>
+            <div class="contentHead">
+                <div class="navbar-menu">
+                    <ul class="navbar-items">
+                        <li class="navbar-items-menu">
+                            <a class="nav-link" href="main.html" onclick="">Páginal Inicial</a>
+                            <select name="departamentos" id="departamentos">
+                                <option value="" disabled selected hidden>Departamentos</option>
+                                <option value="calcados">Calçados</option>
+                                <option value="acessorios">Acessórios</option>
+                                <option value="vestuario">Vestuário</option>
+                                <option value="especiai">Edições Especiais</option>
+                                <option value="marcas">Marcas</option>
+                            </select>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="search">
+                <img id="lupa" src="img/lupa.png" alt="lupa">
+                <input type="text" placeholder="   O que você precisa?" class="ask">
+            </div>
+            <div class="buy">
+                <img id="carrinho" src="img/carrinho.png" alt="">
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="barra">
+            <div id="linhas">
+                <div class="line"></div>
+                <p class="pag">IntenseStreet</p>
+                <div class="line"></div>
+            </div>
+        </div>
+        </div>
+        <div class="slide first">
+            <section class="galery">
+                <div class="fotos">
+                    <img src="img/homepage/1.png" alt="slide1">
+                    <img src="img/homepage/2.png" alt="slide2">
+                    <img src="img/homepage/4.png" alt="slide3">
+                    <img src="img/homepage/3.png" alt="slide4">
+                </div>
+            </section>
+        </div>
+        <div class="marcas">
+            <div class="barra">
+                <div id="linhas">
+                    <div class="line"></div>
+                    <p class="pag">Marcas</p>
+                    <div class="line"></div>
+                </div>
+            </div>
+            <div class="brandimg">
+                <div class="b1"><div class="brand1"><img src="img/homepage/adidas (1).png" alt=""></div> <p>Adidas</p></div>
+                <div class="b1"><div class="brand1"><img src="img/homepage/ous.png" alt=""></div> <p>OUS</p></div>
+                <div class="b1"><div class="brand"><img src="img/homepage/baw.png" alt=""></div> <p>BAW</p></div>
+                <div class="b1"><div class="brand"><img src="img/homepage/nike.png" alt=""></div> <p>Nike</p></div>
+
+            </div>
+        </div>
+
+        <div>
+
+        </div>
+    </main>
+    <footer>
+        
+    </footer>
+    <script src="js/homep.js"></script>
+</body>
+
+</html>
