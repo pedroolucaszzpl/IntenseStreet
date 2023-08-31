13aab2b8bf9d83aad0987c678ab010ddac8c1cac
pedroolucaszzpl <pedrolffarias19@gmail.com>
2023-08-31 20:17:27 -0300

    menu

--- main.php
+++ main.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" /><!--Google Link Icon-->
     <link rel="stylesheet" href="css/main.css">
     <title>IntenseStreet HomePage</title>
 </head>
@@ -20,19 +21,30 @@
                 <div class="navbar-menu">
                     <ul class="navbar-items">
                         <li class="navbar-items-menu">
-                            <a class="nav-link" href="main.html" onclick="">Páginal Inicial</a>
-                            <select name="departamentos" id="departamentos">
+                            <a class="nav-link" href="main.php" onclick="">Páginal Inicial</a></li>
+                            <li><select name="departamentos" id="departamentos">
                                 <option value="" disabled selected hidden>Departamentos</option>
-                                <option value="marcas">Marcas</option>
                                 <option value="calcados">Calçados</option>
                                 <option value="acessorios">Acessórios</option>
                                 <option value="vestuario">Vestuário</option>
-                                <option value="especiai">Edições Especiais</option>
+                                <option value="especiai"><a href="especiais.php">Edições Especiais</a></option>
                                 
                             </select>
                         </li>
                     </ul>
                 </div>
+            </div>
+            <div id="menu_vertical">
+            <span id="icon" class="material-symbols-outlined">
+                menu
+            </span>
+            <ul>
+                <li><a href="main.php">Página Inicial</a></li>
+                <li><a href="main.php">Calçados</a></li>
+                <li><a href="main.php">Acessórios</a></li>
+                <li><a href="main.php">Vestuário</a></li>
+                <li><a href="especiais.php">Edições Especiais</a></li>
+            </ul>
             </div>
             <div class="search">
                 <img id="lupa" src="img/lupa.png" alt="lupa">
