0ce9fbb3ae777ede2f3a06d33501bdf74b7d3364
em1llyVilela <emillyyvilela@gmail.com>
2023-09-01 15:49:44 -0300

    Ajuste Emilly especiais.php e especiais.css

--- main.php
+++ main.php
@@ -14,7 +14,7 @@
 <body>
     <header>
         <nav class="navbar">
-            <a class="navbar-logo" href="index.html">
+            <a class="navbar-logo" href="index.php">
                 <img src="img/logo1.png" alt="Logo IntenseStreet" description="Logo IntenseStreet" id="logo1">
             </a>
             <div class="contentHead">
